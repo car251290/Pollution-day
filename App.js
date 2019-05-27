50ea096b0322c9fb62fbec8f96e76d8de5d1f55a
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2019-05-27 15:55:04 -0700

    first-commit-pollution-day

---
+++ App.js
@@ -0,0 +1,49 @@
+import React, { PureComponent } from 'react';
+import { Constants, Font } from 'expo';
+import { Provider } from 'mobx-react';
+import Sentry from 'sentry-expo';
+
+import { RootStore } from './stores';
+import { Background as LoadingBackground } from './Screens/Loading/Background';
+import { Screens } from './Screens';
+
+// Set up global MST stores
+const stores = RootStore.create({
+  api: undefined,
+  error: undefined,
+  location: {}
+});
+
+// Add sentry if available
+if (Constants.manifest.extra.sentryPublicDsn) {
+  Sentry.config(Constants.manifest.extra.sentryPublicDsn).install();
+}
+
+export class App extends PureComponent {
+  state = {
+    fontLoaded: false
+  };
+
+  async componentDidMount () {
+    // Using custom fonts with Expo
+    // https://docs.expo.io/versions/latest/guides/using-custom-fonts
+    await Font.loadAsync({
+      'gotham-black': require('../assets/fonts/Gotham-Black.ttf'),
+      'gotham-book': require('../assets/fonts/Gotham-Book.ttf')
+    });
+
+    this.setState({ fontLoaded: true });
+  }
+
+  render () {
+    const { fontLoaded } = this.state;
+
+    return fontLoaded ? (
+      <Provider stores={stores}>
+        <Screens />
+      </Provider>
+    ) : (
+      <LoadingBackground />
+    );
+  }
+}
---
+++ BackButton.js
@@ -0,0 +1,37 @@
+import React, { PureComponent } from "react";
+import {Image,StyleSheet,Text,TouchableOpacity, View} from 'react-native';
+
+import backIcon from '../../../assets/images/back.png';
+import * as theme from '../../utils/theme';
+import { i18n } from '../../localization';
+
+export class BackButton extends PureComponent {
+    onClick = () => this.props.onClick();
+
+    render () {
+      const { style } = this.props;
+  
+      return (
+        <View style={style}>
+          <TouchableOpacity
+            hitSlop={{ top: 20, bottom: 20, left: 20, right: 20 }}
+            onPress={this.onClick}
+            style={styles.backButton}
+          >
+            <Image source={backIcon} />
+            <Text style={styles.backText}>{i18n.t('nav_btn_back')}</Text>
+          </TouchableOpacity>
+        </View>
+      );
+    }
+  }
+  const styles = StyleSheet.create({
+      backButton:{
+          alignItems:'center',
+          flexDirection: 'row'
+      },
+      backText: {
+          ...theme.text,
+          marginleft:9
+      }
+  })
